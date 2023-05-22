c8967b488aa371f5d9c32225b6f2bf862dd4888d
Lucas Nogueira <lucasjesusnogueira@gmail.com>
2023-05-22 11:54:24 -0300

    initial commit

---
+++ src/main/java/br/memory/project/lucas/gerenciador_de_funcionarios/GerenciadorDeFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package br.memory.project.lucas.gerenciador_de_funcionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeFuncionariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/memory/project/lucas/gerenciador_de_funcionarios/GerenciadorDeFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.memory.project.lucas.gerenciador_de_funcionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
